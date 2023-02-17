1f02d5578f768d8001dc63328682b6be04914080
EZinkov <evgeniyzinkov98@gmail.com>
2023-02-17 17:17:22 +0100

    Fix horizontal scrollbar

--- src/components/SimilarExercises.js
+++ src/components/SimilarExercises.js
@@ -6,24 +6,18 @@ import Loader from './Loader'
 
 const SimilarExercises = ({targetMuscleExercises, equipmentExercises}) => {
 
-  const handleExerciseClick = () => {
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth',
-    });
-  }
   return (
     <Box sx={{mt: {lg: '100px', xs: '0'}}}>
       <Typography variant='h3' sx={{pl: '20px'}}>
          Exercises that target the same muscle group
-        <Stack direction='row' sx={{p: '22px', position: 'relative'}} onClick={handleExerciseClick}>
+        <Stack direction='row' sx={{p: '22px', position: 'relative'}}>
           {targetMuscleExercises.length ? <HorizontalScrollbar data={targetMuscleExercises} />
           : <Loader />}
         </Stack>
       </Typography>
       <Typography variant='h3' sx={{pl: '20px'}} >
         Exercises that use the same equipment
-        <Stack direction='row' sx={{p: '22px', position: 'relative'}} onClick={handleExerciseClick}>
+        <Stack direction='row' sx={{p: '22px', position: 'relative'}}>
           {equipmentExercises.length ? <HorizontalScrollbar data={equipmentExercises} />
           : <Loader />}
         </Stack>
